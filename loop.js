d036ef9af667b4761afa967de3851d19974b597e
Zoe <zoereagan@gmail.com>
2018-03-13 20:51:28 -0500

    second loop problem complete

--- loop.js
+++ loop.js
@@ -8,3 +8,16 @@ for (let i = 0; i <= 20; i++) {
 		console.log([i] + " is odd.")
 }
 }
+
+//LOOP 2
+for (let i = 1; i <= 100; i++) {
+ 	if (i % 3 === 0 && i % 5 === 0) {
+ 	  console.log("FizzBuzz")
+  } else if (i % 3 === 0) {
+ 	  console.log("Fizz")
+  } else if (i % 5 === 0) {
+ 		console.log("Buzz")
+  } else {
+ 		console.log(i)
+  }
+}
